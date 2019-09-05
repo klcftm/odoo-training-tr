f68cd4b5469e1021b7dccdd7cf1308858e8e5441
Fatma <ftmklc1996@gmail.com>
2019-09-05 09:21:46 +0300

    add it_assignment

---
+++ it_assignment/__manifest__.py
@@ -0,0 +1,26 @@
+# Copyright 2014-2015 Grupo ESOC <www.grupoesoc.es>
+# Copyright 2017-Apertoso N.V. (<http://www.apertoso.be>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Certification",
+    "summary": "Define certification for different purposes",
+    'version': '12.0.1.0.0',
+    "category":"Certification  Management",
+    "website": "https://github.com/klcftm",
+    "author": "Eficent,Odoo Community Association(OCA)",
+    "license": "AGPL-3",
+    "depends": [
+        'base'
+    ],
+    "data": [
+              'security/ir.model.access.csv',
+
+
+
+    ],
+   
+    'development_status':'Beta',
+    'maintainers':['ceeficent'],
+    'installable': True,
+    'application': True,
+}
